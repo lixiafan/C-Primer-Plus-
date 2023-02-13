db25a28be2a5213ad8ccbc1cfe1a5ab18410471b
lixiafan <1993774363@qq.com>
2023-02-13 15:55:35 +0800

    simple code
    第二章的代码

---
+++ 2.1.cpp
@@ -0,0 +1,13 @@
+//myfirst.cpp -- display a message
+/*
+	our first c++ code
+*/
+
+#include<iostream>
+int main(void){
+	using namespace std;
+	cout<<"Come up and C++ me some times.";
+	cout<<endl;
+	cout<<"You won't regret it;"<<endl;
+	return 0;//end of program
+}
